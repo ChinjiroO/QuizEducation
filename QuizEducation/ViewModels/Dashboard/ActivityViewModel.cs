2078a5b478fed5c557213867c4bbcb89aa94cd49
chinjiro <techinkung1452@hotmail.com>
2021-05-09 01:15:25 +0700

    Get Quizzes where equals to current username

---
+++ QuizEducation/ViewModels/Dashboard/ActivityViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using QuizEducation.Helper;
+using Plugin.CloudFirestore;
+using Plugin.FirebaseAuth;
+using QuizEducation.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace QuizEducation.ViewModels.Dashboard
+{
+    public class ActivityViewModel : BaseViewModel
+    {
+        private IPageHelper _pageHelper;
+        private string username;
+        public string Username
+        {
+            get => username;
+            set => SetProperty(ref username, value);
+        }
+
+        public List<Quizzes> _QuizzesList;
+        public List<Quizzes> QuizzesList
+        {
+            get => _QuizzesList;
+            set => SetProperty(ref _QuizzesList, value);
+        }
+
+        public ActivityViewModel(IPageHelper pageHelper)
+        {
+            _pageHelper = pageHelper;
+            GetQuizWhereEqualsToUsername();
+        }
+
+        //---------------------------Methods---------------------------
+
+        private async void GetQuizWhereEqualsToUsername()
+        {
+            var currentUser = CrossFirebaseAuth.Current.Instance.CurrentUser.Uid;
+            var document = await CrossCloudFirestore.Current.Instance.Collection("Users")
+                                                                     .Document(currentUser)
+                                                                     .GetAsync();
+            var user = document.ToObject<Users>();
+            // get name of current user
+            Username = user.name;
+
+            //get data where equals to Username
+            var collection = await CrossCloudFirestore.Current.Instance
+                                                    .Collection("Quizzes")
+                                                    .WhereEqualsTo("createdBy", Username)
+                                                    .GetAsync();
+            var quizModel = collection.ToObjects<Quizzes>().Cast<Quizzes>().ToList();
+            QuizzesList = quizModel;
+        }
+    }
+}
--- QuizEducation/Views/Dashboard/ActivityPage.xaml.cs
+++ QuizEducation/Views/Dashboard/ActivityPage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using QuizEducation.ViewModels;
+using QuizEducation.ViewModels.Dashboard;
+using QuizEducation.ViewModels.Quiz;
+using QuizEducation.Views.Quizzes;
 using Xamarin.Forms;
 
 namespace QuizEducation.Views.Dashboard
@@ -10,6 +14,30 @@ namespace QuizEducation.Views.Dashboard
         public ActivityPage()
         {
             InitializeComponent();
+            var pageHelper = new PageHelper();
+
+            ViewModel = new ActivityViewModel(pageHelper);
+        }
+        public ActivityViewModel ViewModel
+        {
+            get => BindingContext as ActivityViewModel;
+            set => BindingContext = value;
+        }
+    
+
+        private async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.Count == 0)
+                return;
+            if (!(e.CurrentSelection.First() is Models.Quizzes item))
+                return;
+
+            var pageHelper = new PageHelper();
+
+            await pageHelper.PushAsync(new QuizEditorPage(new QuizEditorViewModel(pageHelper)));
+
+            // Manually deselect item.
+            QuizzesListView.SelectedItem = null;
         }
     }
 }
