6571410f47e21e0394b502a28a88f00a90803bcd
chinjiro <techinkung1452@hotmail.com>
2021-05-09 14:55:23 +0700

    Refactoring code (user interface) - add pop async (play quiz) - add method GetCurrentUser (setting page)

--- QuizEducation/ViewModels/Dashboard/SettingViewModel.cs
+++ QuizEducation/ViewModels/Dashboard/SettingViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Windows.Input;
+using Plugin.CloudFirestore;
 using Plugin.FirebaseAuth;
 using QuizEducation.Helper;
+using QuizEducation.Models;
 using QuizEducation.Views.Authentications;
 using Xamarin.Forms;
 
@@ -13,17 +15,53 @@ namespace QuizEducation.ViewModels.Dashboard
         {
             _pageHelper = pageHelper;
             SignOutCommand = new Command(SignOut);
+            GetCurrentUsername();
         }
         //Variable
         private IPageHelper _pageHelper;
+        private string username;
+        private string updUsername;
 
         //Set property
-
+        public string Username
+        {
+            get => username;
+            set => SetProperty(ref username, value);
+        }
+        public string UpdUsername
+        {
+            get => username;
+            set => SetProperty(ref username, value);
+        }
 
         //Command
         public ICommand SignOutCommand { get; }
 
         //Method
+        private async void GetCurrentUsername()
+        {
+            try
+            {
+                var currentUser = CrossFirebaseAuth.Current.Instance.CurrentUser.Uid;
+
+                var document = await CrossCloudFirestore.Current.Instance
+                                                 .Collection("Users")
+                                                 .Document(currentUser)
+                                                 .GetAsync();
+
+                var userModel = document.ToObject<Users>();
+                Username = userModel.name;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private async void UpdateUsername()
+        {
+
+        }
         private void SignOut()
         {
             CrossFirebaseAuth.Current.Instance.SignOut();
--- QuizEducation/ViewModels/Quiz/PlayQuizViewModel.cs
+++ QuizEducation/ViewModels/Quiz/PlayQuizViewModel.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Windows.Input;
 using QuizEducation.Helper;
+using Xamarin.Forms;
 
 namespace QuizEducation.ViewModels.Quiz
 {
     public class PlayQuizViewModel : BaseViewModel
     {
+        public ICommand PopAsyncCommand { get; }
+
+
         // Constructor
         public PlayQuizViewModel(IPageHelper pageHelper)
         {
             _pageHelper = pageHelper;
+            PopAsyncCommand = new Command(PopAsync);
         }
 
         //Variable
         private IPageHelper _pageHelper;
+
+        private async void PopAsync()
+        {
+            await _pageHelper.PopAsync();
+        }
     }
 }
